80dd7a3153c65c1594166fba87ba13413266027c
Douglass Turner <douglass.turner@gmail.com>
2019-11-21 11:14:59 -0500

    add files

--- js/index.js
+++
@@ -1,6 +0,0 @@
-import FileLoadManager from './fileLoadManager.js';
-import FileLoadWidget from './fileLoadWidget.js';
-import MultipleFileLoadController from './multipleFileLoadController.js';
-import TrackLoadController from './trackLoadController.js';
-
-export { FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController }
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import FileLoadManager from './fileLoadManager.js';
+import FileLoadWidget from './fileLoadWidget.js';
+import MultipleFileLoadController from './multipleFileLoadController.js';
+import TrackLoadController, { trackLoadControllerConfigurator } from './trackLoadController.js';
+
+export { FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
