516341dd48da768e6a11d3004123d871f5e1c593
Douglass Turner <douglass.turner@gmail.com>
2019-11-21 10:29:41 -0500

    Initial Build (#1)
    * git ignore-ing
    
    * adding files
    
    * npm run build

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+
+export default [
+    {
+        input: './src/index.js',
+        output:
+            [
+                {
+                    file: 'dist/igv-widgets.js',
+                    format: 'es',
+                    name: 'IGVWidgets'
+                }
+            ]
+    }
+];
