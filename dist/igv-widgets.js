da6fab919fa0e1825526143fa1236c5309216413
Douglass Turner <douglass.turner@gmail.com>
2019-12-04 15:39:11 -0500

    npm'ing

--- dist/igv-widgets.js
+++ dist/igv-widgets.js
@@ -1166,9 +1166,58 @@ function canonicalId(genomeId) {
 
 }
 
+const relativeDOMBBox = (parentElement, childElement) => {
+    const { x: x_p, y: y_p, width: width_p, height: height_p } = parentElement.getBoundingClientRect();
+    const { x: x_c, y: y_c, width: width_c, height: height_c } = childElement.getBoundingClientRect();
+    return { x: (x_c - x_p), y: (y_c - y_p), width: width_c, height:height_c };
+};
+
+function pageCoordinates$1(e) {
+
+    if (e.type.startsWith("touch")) {
+        const touch = e.touches[0];
+        return {x: touch.pageX, y: touch.pageY};
+    } else {
+        return {x: e.pageX, y: e.pageY}
+    }
+}
+
+/**
+ * Translate the mouse coordinates for the event to the coordinates for the given target element
+ * @param e
+ * @param target
+ * @returns {{x: number, y: number}}
+ */
+function translateMouseCoordinates(e, target) {
+
+    var $target = EncodeDataSource(target),
+        posx,
+        posy;
+
+    if (undefined === $target.offset()) {
+        console.log('translateMouseCoordinates - $target.offset() is undefined.');
+    }
+
+    const coords = pageCoordinates$1(e);
+
+    posx = coords.x - $target.offset().left;
+    posy = coords.y - $target.offset().top;
+
+    return {x: posx, y: posy}
+}
+
+
 function guid  () {
     return ("0000" + (Math.random() * Math.pow(36, 4) << 0).toString(36)).slice(-4);
 }
+
+var domUtils$1 = /*#__PURE__*/Object.freeze({
+    __proto__: null,
+    relativeDOMBBox: relativeDOMBBox,
+    pageCoordinates: pageCoordinates$1,
+    translateMouseCoordinates: translateMouseCoordinates,
+    guid: guid
+});
 
 /*
  * The MIT License (MIT)
@@ -10079,4 +10128,4 @@ var urlShortener = /*#__PURE__*/Object.freeze({
     getShortURL: getShortURL
 });
 
-export { Alert, FileLoadManager, FileLoadWidget, fileUtils as FileUtils, appGoogle as GoogleWidgets, MultipleFileLoadController, stringUtils as StringUtils, TrackLoadController, trackUtils as TrackUtils, urlShortener as URLShortener, utils as Utils, igvxhr, oauth, trackLoadControllerConfigurator };
+export { Alert, domUtils$1 as DOMUtils, FileLoadManager, FileLoadWidget, fileUtils as FileUtils, appGoogle as GoogleWidgets, MultipleFileLoadController, stringUtils as StringUtils, TrackLoadController, trackUtils as TrackUtils, urlShortener as URLShortener, utils as Utils, igvxhr, oauth, trackLoadControllerConfigurator };
