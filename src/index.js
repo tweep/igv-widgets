222874a390788ad413dcb080279d45c3b9703c18
Douglass Turner <douglass.turner@gmail.com>
2019-12-05 09:45:27 -0500

    Added more files and Exports. (#15)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import FileLoadManager from './fileLoadManager.js';
 import FileLoadWidget from './fileLoadWidget.js';
 import MultipleFileLoadController from './multipleFileLoadController.js';
 import TrackLoadController, { trackLoadControllerConfigurator } from './trackLoadController.js';
+import { makeDraggable } from "./igvjs/ui/draggable.js";
+import { GenericContainer } from "./igvjs/ui/genericContainer.js";
 import Alert from "./igvjs/ui/alert.js";
 import oauth from "./igvjs/oauth.js";
 import igvxhr from "./igvjs/igvxhr.js";
@@ -12,5 +14,6 @@ import * as GoogleWidgets from './app-google.js';
 import * as FileUtils from './igvjs/util/fileUtils.js';
 import * as URLShortener from './igvjs/urlShortener/urlShortener.js';
 import * as DOMUtils from './igvjs/util/domUtils.js';
+import * as IGVIcons from './igvjs/igv-icons.js';
 
-export { oauth, igvxhr, Alert, DOMUtils, StringUtils, TrackUtils, URLShortener, FileUtils, GoogleWidgets, Utils, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
+export { GenericContainer, makeDraggable, oauth, igvxhr, Alert, IGVIcons, DOMUtils, StringUtils, TrackUtils, URLShortener, FileUtils, GoogleWidgets, Utils, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
