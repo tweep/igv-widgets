373f3a6743a546cd1586ff822122b3149c1bbe35
Douglass Turner <douglass.turner@gmail.com>
2019-12-05 15:48:16 -0500

    IGVColor and IGVMath (#17)

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { GenericContainer } from "./igvjs/ui/genericContainer.js";
 import Alert from "./igvjs/ui/alert.js";
 import oauth from "./igvjs/oauth.js";
 import igvxhr from "./igvjs/igvxhr.js";
+import IGVColor from "./igvjs/igv-color.js";
+import IGVMath from "./igvjs/igv-math.js";
 import * as Utils from './utils.js';
 import * as StringUtils from './igvjs/util/stringUtils.js';
 import * as TrackUtils from './igvjs/util/trackUtils.js';
@@ -15,4 +17,4 @@ import * as FileUtils from './igvjs/util/fileUtils.js';
 import * as URLShortener from './igvjs/urlShortener/urlShortener.js';
 import * as IGVIcons from './igvjs/igv-icons.js';
 
-export { GenericContainer, makeDraggable, oauth, igvxhr, Alert, IGVIcons, StringUtils, TrackUtils, URLShortener, FileUtils, GoogleWidgets, Utils, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
+export { GenericContainer, makeDraggable, oauth, igvxhr, Alert, IGVColor, IGVMath, IGVIcons, StringUtils, TrackUtils, URLShortener, FileUtils, GoogleWidgets, Utils, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
