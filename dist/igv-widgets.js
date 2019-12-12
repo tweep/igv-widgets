333904e325449ab557eed4ad05832c5baef12718
Douglass Turner <douglass.turner@gmail.com>
2019-12-12 16:44:59 -0500

    Juicebox additions (#23)
    * Juicebox additions. EventBus.
    
    * update

--- dist/igv-widgets.js
+++ dist/igv-widgets.js
@@ -9128,7 +9128,7 @@ function parseTabData(data, filter) {
             "Tech Rep": tokens[6],
             "Accession": tokens[11]
         };
-        record["Name"] = constructName(record);
+        record["name"] = constructName(record);
 
         if (filter === undefined || filter(record)) {
             records.push(record);
@@ -21589,6 +21589,95 @@ const GenericContainer = function ({$parent, width, height, closeHandler}) {
 };
 
 /*
+ *  The MIT License (MIT)
+ *
+ * Copyright (c) 2016-2017 The Regents of the University of California
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ * BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+
+/**
+ * @author Jim Robinson
+ */
+
+
+/**
+ * Barebones event bus.
+ */
+
+const EventBus = function () {
+
+
+    // Map eventType -> list of subscribers
+    this.subscribers = {};
+
+    this.stack = [];
+};
+
+EventBus.prototype.subscribe = function (eventType, object) {
+
+    var subscriberList = this.subscribers[eventType];
+    if (subscriberList == undefined) {
+        subscriberList = [];
+        this.subscribers[eventType] = subscriberList;
+    }
+    subscriberList.push(object);
+
+};
+
+EventBus.prototype.post = function (event) {
+
+    const eventType = event.type;
+
+    if (this.hold) {
+        this.stack.push(event);
+    }
+    else {
+        const subscriberList = this.subscribers[eventType];
+
+        if (subscriberList) {
+            for (let subscriber of subscriberList) {
+
+                if ("function" === typeof subscriber.receiveEvent) {
+                    subscriber.receiveEvent(event);
+                } else if ("function" === typeof subscriber) {
+                    subscriber(event);
+                }
+            }
+        }
+    }
+};
+
+EventBus.prototype.hold = function () {
+    this.hold = true;
+
+};
+
+EventBus.prototype.release = function () {
+    this.hold = false;
+    for (let event of this.stack) {
+        this.post(event);
+    }
+    this.stack = [];
+};
+
+/*
  * The MIT License (MIT)
  *
  * Copyright (c) 2016-2017 The Regents of the University of California
@@ -22755,4 +22844,4 @@ var widgetUtils = /*#__PURE__*/Object.freeze({
     createColorSwatchSelector: createColorSwatchSelector
 });
 
-export { Alert, FileLoadManager, FileLoadWidget, fileUtils as FileUtils, GenericContainer, appGoogle as GoogleWidgets, IGVColor, igvIcons as IGVIcons, IGVMath, MultipleFileLoadController, stringUtils as StringUtils, TrackLoadController, trackUtils as TrackUtils, urlShortener as URLShortener, utils as Utils, widgetUtils as WidgetUtils, google$1 as google, igvxhr, makeDraggable, oauth, trackLoadControllerConfigurator };
+export { Alert, EventBus, FileLoadManager, FileLoadWidget, fileUtils as FileUtils, GenericContainer, appGoogle as GoogleWidgets, IGVColor, igvIcons as IGVIcons, IGVMath, MultipleFileLoadController, stringUtils as StringUtils, TrackLoadController, trackUtils as TrackUtils, urlShortener as URLShortener, utils as Utils, widgetUtils as WidgetUtils, google$1 as google, igvxhr, makeDraggable, oauth, trackLoadControllerConfigurator };
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import MultipleFileLoadController from './multipleFileLoadController.js';
 import TrackLoadController, { trackLoadControllerConfigurator } from './trackLoadController.js';
 import { makeDraggable } from "./igvjs/ui/draggable.js";
 import { GenericContainer } from "./igvjs/ui/genericContainer.js";
+import EventBus from './jb/eventBus.js';
 import Alert from "./igvjs/ui/alert.js";
 import oauth from "./igvjs/oauth.js";
 import igvxhr from "./igvjs/igvxhr.js";
@@ -18,4 +19,4 @@ import * as FileUtils from './igvjs/util/fileUtils.js';
 import * as URLShortener from './igvjs/urlShortener/urlShortener.js';
 import * as IGVIcons from './igvjs/igv-icons.js';
 import * as WidgetUtils from './widgetUtils.js'
-export { GenericContainer, makeDraggable, google, oauth, igvxhr, Alert, IGVColor, IGVMath, WidgetUtils, IGVIcons, StringUtils, TrackUtils, URLShortener, FileUtils, GoogleWidgets, Utils, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
+export { GenericContainer, makeDraggable, google, oauth, igvxhr, EventBus, Alert, IGVColor, IGVMath, WidgetUtils, IGVIcons, StringUtils, TrackUtils, URLShortener, FileUtils, GoogleWidgets, Utils, FileLoadManager, FileLoadWidget, MultipleFileLoadController, TrackLoadController, trackLoadControllerConfigurator }
