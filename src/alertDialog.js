4cd44948325f673d1494731b49e9ec1cff51abbb
Douglass Turner <douglass.turner@gmail.com>
2020-07-31 09:19:46 -0400

    rename (#42)

--- src/alertDialog.js
+++ src/alertDialog.js
@@ -12,7 +12,7 @@ class AlertDialog {
     constructor(parent) {
 
         // container
-        this.container = DOMUtils.div({class: "igv-ui-alert-dialog-container"});
+        this.container = DOMUtils.div({class: "igv-widgets-alert-dialog-container"});
         parent.appendChild(this.container);
 
         // header
@@ -20,11 +20,11 @@ class AlertDialog {
         this.container.appendChild(header);
 
         // body container
-        let bodyContainer = DOMUtils.div({id: 'igv-ui-alert-dialog-body'});
+        let bodyContainer = DOMUtils.div({id: 'igv-widgets-alert-dialog-body'});
         this.container.appendChild(bodyContainer);
 
         // body copy
-        this.body = DOMUtils.div({id: 'igv-ui-alert-dialog-body-copy'});
+        this.body = DOMUtils.div({id: 'igv-widgets-alert-dialog-body-copy'});
         bodyContainer.appendChild(this.body);
 
         // ok container
@@ -35,14 +35,14 @@ class AlertDialog {
         this.ok = DOMUtils.div();
         ok_container.appendChild(this.ok);
         this.ok.textContent = 'OK';
-        const self = this;
-        this.ok.addEventListener('click', function (ev) {
-            if (typeof self.callback === 'function') {
-                self.callback("OK");
-                self.callback = undefined;
+
+        this.ok.addEventListener('click', () => {
+            if (typeof this.callback === 'function') {
+                this.callback("OK");
+                this.callback = undefined;
             }
-            self.body.innerHTML = '';
-            DOMUtils.hide(self.container);
+            this.body.innerHTML = '';
+            DOMUtils.hide(this.container);
         });
 
         makeDraggable(this.container, header);
